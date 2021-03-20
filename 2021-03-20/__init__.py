374372b0770283b86f67591ab581f1d275709164
Kevain <522516064#qq.com>
2021-03-20 16:23:50 +0800

    2021-03-20

---
+++ 2021-03-20/__init__.py
@@ -0,0 +1,7 @@
+"""
+@FileName：__init__.py.py
+@Description：...
+@Author：Ken
+@Time：2021/3/20 14:41
+"""
+
---
+++ 2021-03-20/lesson1-pytho语法.py
@@ -0,0 +1,15 @@
+"""
+@FileName：lesson1-pytho语法.py
+@Description：python基本语法
+@Author：Ken
+@Time：2021/3/20 14:44
+"""
+
+# 注释快捷键 ctrl+/
+"""
+多行注释  ”“”
+查看源码 ctrl+B
+调整代码格式 alt+ctrl+L
+"""
+print("hello,world123")
+
