bd716fdbdda86c9c6dafb87c56e617eac39b5d49
Kevain <522516064#qq.com>
2021-03-29 22:27:03 +0800

    2021-03-20
    Signed-off-by: Kevain <522516064#qq.com>

--- 2021-03-20/lesson1-pytho语法.py
+++ 2021-03-20/lesson1-pytho语法.py
@@ -13,3 +13,14 @@
 """
 print("hello,world123")
 
+# age = 18.5
+# age_str = "18.5"
+# age_str2 = '18.5'
+# person_info = "你好，我叫XXx，我来自XXX,我现在工作" \
+#               "是XXx，我的项目是XXX,我负责的XXX"
+# print(person_info[1::2])
+# a=person_info.find("X")
+# print(a)
+age = input("age:")
+name = input("name:")
+print("{}的年龄是{}岁".format(name, age))
