9cb7c407e0930c64664eb402b2235f5a85355541
Kevain <522516064#qq.com>
2021-04-09 00:07:48 +0800

    2021-03-20
    Signed-off-by: Kevain <522516064#qq.com>

---
+++ 2021-04-08/lesson1-4.py
@@ -0,0 +1,18 @@
+"""
+@FileName：lesson1-4.py
+@Description：...
+@Author：Ken
+@Time：2021/4/9 0:03
+if-elis
+"""
+print("请输入数字")
+a = input()  # 从控制台输入内容放到变量A
+flag = int(a)  # 输入的内容默认是int
+if flag == 1:
+    print("1111")
+elif flag == 2:
+    print("张三")
+elif flag == 4:
+    print("linda")
+else:
+    print("qita")
